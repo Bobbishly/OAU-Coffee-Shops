b7464ec12bc81e5b70675722bd7b9d829a757f81
Bobbishly <ayomiderichard255@gmail.com>
2024-12-14 19:35:52 +0100

    Uploading Files

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-14">
+        <h1>Coffee Shops</h1>
+      </main>
+  );
+}
