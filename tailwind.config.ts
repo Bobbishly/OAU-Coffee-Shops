a8a8ac452754bd225cc84e8f618f0bf1f9b38c28
Bobbishly <ayomiderichard255@gmail.com>
2024-12-16 13:56:57 +0100

    Uploading Files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-        backgroundImage: {
-          gradient: "url('/static/background.png')",
+      backgroundImage: {
+        gradient: "url('/static/background.png')",
+      },
+      colors: {
+        purple: {
+          951: 'rgba(79, 70, 229, 1)',
         },
-        colors: {
-          purple: {
-            951: 'rgba(79, 70, 229, 1)',
-          },
-          gray: {
-            950: 'hsla(0, 0%, 100%, 0.7);',
-            951: 'hsla(0, 0%, 100%, 0.4);',
-            952: '#373b64',
-          },
+        gray: {
+          950: 'hsla(0, 0%, 100%, 0.7);',
+          951: 'hsla(0, 0%, 100%, 0.4);',
+          952: '#373b64',
         },
+      },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
