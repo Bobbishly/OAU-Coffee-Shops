3b0ac09a90d5e85a7dfd5c8061e452e1c9381383
Bobbishly <ayomiderichard255@gmail.com>
2024-12-16 11:23:07 +0100

    Uploading Files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 import Banner from "@/components/banner.client";
-import Link from "next/link";
+// import Link from "next/link";
+import Card from "@/components/card.server";
 
 export default function Home() {
   const coffeeStoreId = 'dark-horse-coffee'
@@ -8,7 +9,13 @@ export default function Home() {
     <div className="mb-56">
       <main className="mx-auto mt-10 max-w-6xl px-4">
           <Banner />
-          <Link href={`/coffee-store/${coffeeStoreId}`}> Dark Horse Coffee </Link>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
+            <Card 
+              name="Parkhouse Coffee"
+              imgUrl="/static/hero-image.png"
+              href={`/coffee-store/${coffeeStoreId}`}
+            />
+          </div>
         </main>
     </div>
   );
